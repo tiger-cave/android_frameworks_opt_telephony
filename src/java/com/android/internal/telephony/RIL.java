65bdf31e6f6895a01e08b3664666ff1fd8601848
Bruno Martins <bgcngm@gmail.com>
2017-09-06 12:06:09 +0200

    telephony: Squashed support for simactivation feature
     * This is a squash of the "OldRilFeatures" implementation plus
       the following changes, forward-ported to R.
    
       Author: Danny Baumann <dannybaumann@web.de>
       Date:   Mon Dec 7 16:37:45 2015 +0100
    
           Also activate default UICC apps once after SIM presence.
    
           Some devices apparently need this, and it shouldn't hurt on other
           devices either.
    
           Change-Id: I7feb27cceeffc30081e682a6f6b0e2bdb6ad5087
    
       Author: Danny Baumann <dannybaumann@web.de>
       Date:   Thu Nov 5 11:06:09 2015 +0100
    
           Activate default UICC application if not done automatically.
    
           Provides compatibility to the pre-M behaviour.
    
           Change-Id: Ie87e40cf1da3b19fd37e2c07ff50845f0842216a
    
    Change-Id: I785a91dadd79713f57641a3c20a94a0220a723e9
    (cherry picked from commit 45ba03961385e76220d9e6ee952d9478dbb4d36b)

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -255,6 +255,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
     AtomicBoolean mTestingEmergencyCall = new AtomicBoolean(false);
 
     final Integer mPhoneId;
+    private List<String> mOldRilFeatures;
 
     private boolean mUseOldMncMccFormat;
 
@@ -624,6 +625,9 @@ public class RIL extends BaseCommands implements CommandsInterface {
 
         mUseOldMncMccFormat = SystemProperties.getBoolean(
                 "ro.telephony.use_old_mnc_mcc_format", false);
+
+        final String oldRilFeatures = SystemProperties.get("ro.telephony.ril.config", "");
+        mOldRilFeatures = Arrays.asList(oldRilFeatures.split(","));
 
         TelephonyManager tm = (TelephonyManager) context.getSystemService(
                 Context.TELEPHONY_SERVICE);
@@ -4688,8 +4692,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
     }
 
     @Override
-    public void setUiccSubscription(int slotId, int appIndex, int subId,
-                                    int subStatus, Message result) {
+    public void setUiccSubscription(int appIndex, boolean activate, Message result) {
         IRadio radioProxy = getRadioProxy(result);
         if (radioProxy != null) {
             RILRequest rr = obtainRequest(RIL_REQUEST_SET_UICC_SUBSCRIPTION, result,
@@ -4697,15 +4700,14 @@ public class RIL extends BaseCommands implements CommandsInterface {
 
             if (RILJ_LOGD) {
                 riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
-                        + " slot = " + slotId + " appIndex = " + appIndex
-                        + " subId = " + subId + " subStatus = " + subStatus);
+                        + " appIndex: " + appIndex + " activate: " + activate);
             }
 
             SelectUiccSub info = new SelectUiccSub();
-            info.slot = slotId;
+            info.slot = mPhoneId;
             info.appIndex = appIndex;
-            info.subType = subId;
-            info.actStatus = subStatus;
+            info.subType = mPhoneId;
+            info.actStatus = activate ? 1 : 0;
 
             try {
                 radioProxy.setUiccSubscription(rr.mSerial, info);
@@ -7823,5 +7825,9 @@ public class RIL extends BaseCommands implements CommandsInterface {
      */
     public HalVersion getHalVersion() {
         return mRadioVersion;
+    }
+
+    public boolean needsOldRilFeature(String feature) {
+        return mOldRilFeatures.contains(feature);
     }
 }
