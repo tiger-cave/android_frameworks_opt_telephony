bdd2fbf82da0545689b9e8930920a5e8176f00a9
GuaiYiHu <guaiyihu@foxmail.com>
2026-08-10 11:35:31 +0800

    Telephony: restore OEM hook command interfaces
    Change-Id: Ic1919a6c0cda22f1dd257a81f081f0b07ab99d2d
    (cherry picked from commit 1d60731efea0286fb2cef55612e22c476d7a2356)

--- src/java/com/android/internal/telephony/CommandsInterface.java
+++ src/java/com/android/internal/telephony/CommandsInterface.java
@@ -1628,6 +1628,9 @@ public interface CommandsInterface {
     @UnsupportedAppUsage
     void reportStkServiceIsRunning(Message result);
 
+    @UnsupportedAppUsage
+    void invokeOemRilRequestRaw(byte[] data, Message response);
+
     /**
      * Sends carrier specific information to the vendor ril that can be used to
      * encrypt the IMSI and IMPI.
@@ -1636,6 +1639,14 @@ public interface CommandsInterface {
      * @param response callback message
      */
     void setCarrierInfoForImsiEncryption(ImsiEncryptionInfo imsiEncryptionInfo, Message response);
+
+    void invokeOemRilRequestStrings(String[] strings, Message response);
+
+    /**
+     * Fires when RIL_UNSOL_OEM_HOOK_RAW is received from the RIL.
+     */
+    void setOnUnsolOemHookRaw(Handler h, int what, Object obj);
+    void unSetOnUnsolOemHookRaw(Handler h);
 
     /**
      * Send TERMINAL RESPONSE to the SIM, after processing a proactive command
--- tests/telephonytests/src/com/android/internal/telephony/SimulatedCommands.java
+++ tests/telephonytests/src/com/android/internal/telephony/SimulatedCommands.java
@@ -1549,6 +1549,22 @@ public class SimulatedCommands extends BaseCommands
         }
     }
 
+    @Override
+    public void invokeOemRilRequestRaw(byte[] data, Message response) {
+        if (response != null) {
+            AsyncResult.forMessage(response).result = data;
+            response.sendToTarget();
+        }
+    }
+
+    @Override
+    public void invokeOemRilRequestStrings(String[] strings, Message response) {
+        if (response != null) {
+            AsyncResult.forMessage(response).result = strings;
+            response.sendToTarget();
+        }
+    }
+
     //***** SimulatedRadioControl
 
 
